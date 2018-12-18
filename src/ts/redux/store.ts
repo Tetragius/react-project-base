245bb41794583215faa99230e547c590225c1dcc
Chertorylskiy Ilya <tetragius@gmail.com>
2018-12-19 00:25:17 +0300

    redux devtool

--- src/ts/redux/store.ts
+++ src/ts/redux/store.ts
@@ -1,10 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 import { createHashHistory } from 'history';
 import { routerMiddleware, routerReducer } from 'react-router-redux';
 
-export const history = createHashHistory({ basename: '/' })
-const middleware = routerMiddleware(history)
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const history = createHashHistory({ basename: '/' });
+const middleware = routerMiddleware(history);
 
 const createReducer = (asyncReducers?) => {
     return combineReducers<any>({
@@ -16,7 +17,7 @@ const createReducer = (asyncReducers?) => {
 
 export const store = createStore(
     createReducer(),
-    applyMiddleware(middleware)
+    composeEnhancers(applyMiddleware(middleware))
 );
 
 export const injectAsyncReducer = (name: string, asyncReducer: any) => {
